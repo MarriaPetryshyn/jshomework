2ee23a439cad6579a54a8cbf2dc829e578c0bc96
MarriaPetryshyn <petryshyn.main@gmail.com>
2017-05-17 18:23:32 +0300

    added js code

---
+++ script1.js
@@ -0,0 +1,8 @@
+alert('Please enter your name, preferred time and place to organize an event. ');
+
+var name = prompt('Please enter your first name');
+var someEvent = prompt('Please enter your preferred type of event');
+var  time = prompt('Please enter your preferred time');
+var place = prompt('Please enter your preferred place');
+
+console.log(name, 'has', someEvent, 'at', time, 'somewhere in(at)', place);
